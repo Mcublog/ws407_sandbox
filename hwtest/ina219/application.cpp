f30de957af0f307fe95174e873c4b0bfe75e44bc
Viacheslav <slava.k@ks2corp.com>
2024-08-11 14:35:00 +0300

    reading current ina219

--- hwtest/ina219/application.cpp
+++ hwtest/ina219/application.cpp
@@ -14,6 +14,7 @@
 #include "app/version.h"
 #include "app/utils/delay.h"
 #include "app/io/io_gpio.h"
+#include "libs/third_party/hw/PSM_INA219_STM32/INA219.h"
 //>>---------------------- Log control
 #define LOG_MODULE_NAME app
 #if defined(NDEBUG)
@@ -28,11 +29,19 @@ void application()
 {
     LOG_INFO("Version: %s", FW_VERSION);
     bool led = false;
+
+    INA219_t ina219 = {};
+    int err = INA219_Init(&ina219, &hi2c1, INA219_ADDRESS);
+    if (err == 0)
+        LOG_ERROR("INA219_Init");
+
     while (1)
     {
-        delay_ms(500);
+        delay_ms(200);
+        uint16_t voltage = INA219_ReadShuntVolage_mV(&ina219);
+        int16_t current = INA219_ReadCurrent_raw(&ina219);
+        LOG_INFO("current: %d", current);
         io_gpio_led(led);
         led ^= true;
-        LOG_INFO("working...");
     }
 }
