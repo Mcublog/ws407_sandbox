e7c75452eca03c3816a4a534d2ffdb6910bea940
Viacheslav <slava.k@ks2corp.com>
2024-08-12 11:00:39 +0300

    changed ina driver

--- hwtest/ina219/application.cpp
+++ hwtest/ina219/application.cpp
@@ -8,13 +8,16 @@
  * @copyright Viacheslav mcublog (c) 2024
  *
  */
+#include <cmath>
+
 #include "main.h"
 #include "i2c.h"
 #include "application.h"
 #include "app/version.h"
 #include "app/utils/delay.h"
 #include "app/io/io_gpio.h"
-#include "libs/third_party/hw/PSM_INA219_STM32/INA219.h"
+#include "libs/third_party/hw/ina219/src/driver_ina219.h"
+#include "libs/third_party/hw/ina219/interface/driver_ina219_interface.h"
 //>>---------------------- Log control
 #define LOG_MODULE_NAME app
 #if defined(NDEBUG)
@@ -30,17 +33,71 @@ void application()
     LOG_INFO("Version: %s", FW_VERSION);
     bool led = false;
 
-    INA219_t ina219 = {};
-    int err = INA219_Init(&ina219, &hi2c1, INA219_ADDRESS);
-    if (err == 0)
-        LOG_ERROR("INA219_Init");
+    static ina219_handle_t gs_handle;
+
+    /* link interface function */
+    DRIVER_INA219_LINK_INIT(&gs_handle, ina219_handle_t);
+    DRIVER_INA219_LINK_IIC_INIT(&gs_handle, ina219_interface_iic_init);
+    DRIVER_INA219_LINK_IIC_DEINIT(&gs_handle, ina219_interface_iic_deinit);
+    DRIVER_INA219_LINK_IIC_READ(&gs_handle, ina219_interface_iic_read);
+    DRIVER_INA219_LINK_IIC_WRITE(&gs_handle, ina219_interface_iic_write);
+    DRIVER_INA219_LINK_DELAY_MS(&gs_handle, ina219_interface_delay_ms);
+    DRIVER_INA219_LINK_DEBUG_PRINT(&gs_handle, ina219_interface_debug_print);
+
+    uint8_t res = ina219_set_addr_pin(&gs_handle, INA219_ADDRESS_0);
+    if (res != 0)
+        LOG_ERROR("ina219: set addr pin failed");
+
+    res = ina219_set_resistance(&gs_handle, 0.1);
+    if (res != 0)
+        LOG_ERROR("ina219: set resistance");
+
+    res = ina219_init(&gs_handle);
+    if (res != 0)
+        LOG_ERROR("ina219: ina219_init");
+
+    res = ina219_set_bus_voltage_range(&gs_handle, INA219_BUS_VOLTAGE_RANGE_32V);
+    if (res != 0)
+        LOG_ERROR("ina219: ina219_set_bus_voltage_range");
+
+    res = ina219_set_bus_voltage_adc_mode(&gs_handle, INA219_ADC_MODE_12_BIT_64_SAMPLES);
+    if (res != 0)
+        LOG_ERROR("ina219: ina219_set_bus_voltage_adc_mode");
+
+    res = ina219_set_shunt_voltage_adc_mode(&gs_handle, INA219_ADC_MODE_12_BIT_64_SAMPLES);
+    if (res != 0)
+        LOG_ERROR("ina219: ina219_set_shunt_voltage_adc_mode");
+
+    res = ina219_set_mode(&gs_handle, INA219_MODE_SHUNT_BUS_VOLTAGE_CONTINUOUS);
+    if (res != 0)
+        LOG_ERROR("ina219: ina219_set_mode");
+
+    res = ina219_set_pga(&gs_handle, INA219_PGA_320_MV);
+    if (res != 0)
+        LOG_ERROR("ina219: ina219_set_pga");
+
+    uint16_t calibration = 4096;
+    res = ina219_calculate_calibration(&gs_handle, (uint16_t *)&calibration);
+    if (res != 0)
+        LOG_ERROR("ina219: ina219_calculate_calibration");
+
+    LOG_INFO("ina219: calibration: %d", calibration);
+
+    res = ina219_set_calibration(&gs_handle, calibration);
+    if (res != 0)
+        LOG_ERROR("ina219: ina219_set_calibration");
 
     while (1)
     {
-        delay_ms(200);
-        uint16_t voltage = INA219_ReadShuntVolage_mV(&ina219);
-        int16_t current = INA219_ReadCurrent_raw(&ina219);
-        LOG_INFO("current: %d", current);
+        delay_ms(1000);
+        int16_t rawA = 0, rawV = 0;
+        float current, voltage = 0.0f;
+        res = ina219_read_current(&gs_handle, &rawA, &current);
+        res |= ina219_read_shunt_voltage(&gs_handle, &rawV, &voltage);
+        if (res != 0)
+            LOG_ERROR("ina219: reading");
+        LOG_INFO("raw[%d] current: %d mA", rawA, (int)roundf(current));
+        LOG_INFO("raw[%d] voltage: %d mV", rawV, (int)roundf(voltage));
         io_gpio_led(led);
         led ^= true;
     }
--- libs/third_party/hw/list.cmake
+++ libs/third_party/hw/list.cmake
@@ -6,6 +6,10 @@ file(GLOB_RECURSE SSD1309_SOURCES
     ${PROJECT_SOURCE_DIR}/libs/third_party/hw/stm32-ssd1306/ssd1306/*.c
 )
 
-file(GLOB_RECURSE INA219_SOURCES
+file(GLOB_RECURSE PSM_INA219_STM32_SOURCES
     ${PROJECT_SOURCE_DIR}/libs/third_party/hw/PSM_INA219_STM32/INA219.c
+)
+
+file(GLOB_RECURSE INA219_SOURCES
+    ${PROJECT_SOURCE_DIR}/libs/third_party/hw/ina219/src/driver_ina219.c
 )
