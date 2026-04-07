17b020056841d6cb9f462cb59344eef0774fd67f
Viacheslav <viacheslav@mcublog.ru>
2026-04-07 22:26:30 +0300

    hwtest: i2c_slave: initial ping pong;

--- hwtest/i2c_slave/application.cpp
+++ hwtest/i2c_slave/application.cpp
@@ -33,35 +33,110 @@ extern I2C_HandleTypeDef hi2c2;
 uint8_t txData[] = "Hello from I2C1";
 uint8_t rxData[32] = {};
 
+uint8_t slaveTxData[] = "STM32 I2C2 OK";
+uint8_t masterRxData[32];
+
+
 #define kSlaveAddr (2U << 1U)
 
 volatile bool m_data_rx = false;
+volatile bool masterWaitData = false;
+volatile bool slaveWaitData = false;
+
+I2C_HandleTypeDef *i2cMasterHandler = &hi2c1;
+I2C_HandleTypeDef *i2cSlaveHandler = &hi2c2;
 //<<----------------------
 void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c)
 {
     if(hi2c->Instance == I2C2)
     {
+        LOG_INFO("HAL_I2C_SlaveRxCpltCallback");
         m_data_rx = true;
-        // if(TransferDirection == I2C_DIRECTION_TRANSMIT)
-        // {
-        //     // мастер пишет в slave
-        //     HAL_I2C_Slave_Receive_IT(hi2c, rxData, sizeof(rxData));
-        // }
-        // else
-        // {
-        //     // мастер читает из slave
-        //     HAL_I2C_Slave_Transmit_IT(hi2c, rxData, sizeof(rxData));
-        // }
+        slaveWaitData = false;
     }
 }
 
-void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c)
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+  if (hi2c == i2cMasterHandler)
+  {
+    LOG_INFO("HAL_I2C_MasterRxCpltCallback");
+    masterWaitData = 0;
+  }
+}
+
+void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c,
+                          uint8_t TransferDirection,
+                          uint16_t AddrMatchCode)
 {
     if(hi2c->Instance == I2C2)
     {
-        // обязательно снова включить listen
-        HAL_I2C_EnableListen_IT(hi2c);
+        LOG_INFO("HAL_I2C_AddrCallback: %d a: 0x%02x", TransferDirection, AddrMatchCode);
+        if(TransferDirection == I2C_DIRECTION_RECEIVE)
+        {
+            // мастер читает у slave
+            // HAL_I2C_Slave_Transmit(hi2c, slaveTxData, sizeof(slaveTxData), 10);
+            HAL_I2C_Slave_Transmit_IT(hi2c,
+                                      slaveTxData,
+                                      sizeof(slaveTxData));
+        }
+        else
+        {
+            HAL_I2C_Slave_Receive_IT(hi2c, rxData, sizeof(rxData));
+        }
     }
+}
+
+void HAL_I2C_SlaveTxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+    if(hi2c->Instance == I2C2)
+    {
+        // можно обновить данные перед следующим чтением
+        // например счётчик, регистр и т.п.
+        LOG_INFO("HAL_I2C_SlaveTxCpltCallback");
+    }
+}
+
+void master_read(bool &led)
+{
+    delay_ms(500);
+    io_gpio_led(led);
+    led ^= true;
+
+    slaveWaitData = 1;
+    HAL_I2C_Slave_Receive_IT(i2cSlaveHandler, rxData, sizeof(txData));
+    HAL_I2C_Master_Transmit(&hi2c1, kSlaveAddr, txData, sizeof(txData), HAL_MAX_DELAY);
+    while(slaveWaitData);
+    LOG_INFO("slave rx done: %s", rxData);
+
+    masterWaitData = 1;
+    HAL_I2C_Master_Receive_IT(i2cMasterHandler, kSlaveAddr, masterRxData, sizeof(slaveTxData));
+    HAL_I2C_Slave_Transmit_IT(i2cSlaveHandler, slaveTxData, sizeof(slaveTxData));
+    while(masterWaitData);
+
+    LOG_INFO("master rx done: %s", masterRxData);
+    memset(masterRxData, 0, sizeof(masterRxData));
+
+}
+
+void master_write(bool &led)
+{
+    HAL_I2C_Slave_Receive_IT(&hi2c2, rxData, sizeof(txData));
+
+    delay_ms(500);
+    io_gpio_led(led);
+    led ^= true;
+
+    auto s = HAL_I2C_Master_Transmit(&hi2c1, kSlaveAddr, txData, sizeof(txData), HAL_MAX_DELAY);
+    LOG_INFO("i2c send status: %d", s);
+    while (!m_data_rx)
+    {
+        /* code */
+    }
+    // здесь данные уже в rxData
+    LOG_INFO("rx: %s", rxData);
+    memset(rxData, 0, sizeof(rxData));
+    m_data_rx = false;
 }
 
 void application()
@@ -71,27 +146,7 @@ void application()
 
     while (1)
     {
-        HAL_I2C_Slave_Receive_IT(&hi2c2, rxData, sizeof(txData));
-
-        delay_ms(500);
-        io_gpio_led(led);
-        led ^= true;
-
-        auto s = HAL_I2C_Master_Transmit(&hi2c1, kSlaveAddr, txData, sizeof(txData), HAL_MAX_DELAY);
-        LOG_INFO("i2c send status: %d", s);
-        while (!m_data_rx)
-        {
-            /* code */
-        }
-        // здесь данные уже в rxData
-        LOG_INFO("rx: %s", rxData);
-        memset(rxData, 0, sizeof(rxData));
-
-        // перезапустить прием
-        // HAL_I2C_DeInit(&hi2c2);
-        // MX_I2C2_Init();
-        //HAL_NVIC_EnableIRQ(I2C2_EV_IRQn);
-        // HAL_I2C_Slave_Receive_IT(&hi2c2, rxData, sizeof(rxData));
-        m_data_rx = false;
+        // master_read(led);
+        master_write(led);
     }
 }
